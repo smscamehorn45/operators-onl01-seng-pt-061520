ca5717760012ae50a44abf5f4967c3742fb53f29
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-26 23:04:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,6 @@
 
 def unsafe?(speed)
+<<<<<<< HEAD
   if speed < 40 
     return true
     elsif speed > 60 
@@ -10,6 +11,15 @@ elsif speed > 40 && speed < 60
 return false
 end
  end
+=======
+ if speed > 60 && < 40
+   return true
+elsif speed < 60 && > 40
+return false
+ 
+ end
+ 
+>>>>>>> 954edccc1e18381418b5905f2cc96ba3d5db789b
 
 
 
